fef184bbab6cc871461c2ae0e2cfb27d5de29748
anjaliprt <anjalir303@gmail.com>
2024-06-04 10:44:15 +0400

    updated url in weather service

--- src/main/java/com/app/WeatherAPI/controller/WeatherController.java
+++ src/main/java/com/app/WeatherAPI/controller/WeatherController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://global-weather-search-app.netlify.app/")
 
 public class WeatherController {
 
