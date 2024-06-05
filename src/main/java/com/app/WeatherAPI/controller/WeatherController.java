d6b0974a95ab75947c8fea5d8507d87b91dd1669
Anjali Ramachandran <anjalir303@gmail.com>
2024-06-05 11:23:35 +0400

    updated controller

--- src/main/java/com/app/WeatherAPI/controller/WeatherController.java
+++ src/main/java/com/app/WeatherAPI/controller/WeatherController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://weather-application-home.onrender.com/")
+@CrossOrigin(origins = "https://weather-application-home.onrender.com")
 
 public class WeatherController {
 
